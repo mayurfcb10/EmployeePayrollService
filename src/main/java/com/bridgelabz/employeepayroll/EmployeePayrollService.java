f4ceb9023443ad03cf68eedf72b5a71147cc3b8d
Mayur <mayurzope555@gmail.com>
2020-10-14 17:36:59 +0530

    [Mayur]Added: Welcome message for Employee Payroll Service program

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayrollService {
+	public void printWelcomeMessage() {
+		System.out.println("Welcome to the Employee PayRoll Service Program");
+	}
+}
